f08e3602c2da3d361b9bc0d6231db492517cd9fe
Hrutav Modha <modhahrutav@gmail.com>
2026-05-26 18:49:19 +0530

    Refined docs

--- compile.sh
+++ compile.sh
@@ -7,4 +7,3 @@ npx tsc
 # Compile research paper
 pdflatex -output-directory dist/docs -jobname research-paper docs/research-paper.tex
 pdflatex -output-directory dist/docs -jobname research-paper docs/research-paper.tex
-
